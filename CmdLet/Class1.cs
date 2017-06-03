2553d817dcf57bbc5cbf6fae80df3f7d474c0c62
Baurzhan Zhylkaidarov <Baurzhan Zhylkaidarov>
2017-06-03 09:10:46 -0400

    Add project files.

---
+++ CmdLet/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management.Automation;
+
+
+
+
+namespace CmdLet
+{
+    [Cmdlet(VerbsCommon.Get, "NetworkAdapter")]
+    [OutputType(typeof(NetworkAdapter))]
+    public class GetExcerciseEmCmdlet: Cmdlet
+    {
+        [Parameter]
+        public string Name { get; set; }
+        [Parameter]
+        public string Manufacturer { get; set; }
+        [Parameter]
+        public bool PhysicalAdapter { get; set; }
+        [Parameter]
+        public int MaxEntries { get; set; }
+    }
+    public class NetworkAdapter
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int DeviceId { get; set; }
+        public string Manufacturer { get; set; }
+        public string NetConnectionId { get; set; }
+        public bool PhysicalAdapter { get; set; }
+    }
+}
